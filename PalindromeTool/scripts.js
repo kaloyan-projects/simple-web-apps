34b50e543a785c9a1b875f716dd2aef18b6608e7
Калоян <kaloyansivanov@proton.me>
2024-11-04 18:49:18 +0200

    Add files via upload

---
+++ PalindromeTool/scripts.js
@@ -0,0 +1,33 @@
+function reverse_s(s){
+    return s.split("").reverse().join("");
+}
+
+var changesMade = false;
+
+var message = document.getElementById("ispalendrome");
+var reversed = document.getElementById("reversed");
+check_for_palendrome();
+inputField.addEventListener('input', () => {
+    check_for_palendrome();
+    changesMade = true;
+});
+
+function check_for_palendrome() {
+    var text = inputField.value;
+    reversed.innerHTML = "Reversed text: " + reverse_s(text);
+    var text = text.toLowerCase().replace(/[^\p{L}]/gu, '');
+    if (text == reverse_s(text) && text.length != 0) {
+        message.innerHTML = "This is a palendrome.";
+        message.style.color = "green";
+    } else {
+        message.innerHTML = "This is not a palendrome.";
+        message.style.color = "red";
+    }
+
+}
+window.addEventListener("beforeunload", function (e) {
+    if (changesMade) {
+        e.preventDefault();
+        e.returnValue = '';
+    }
+});
